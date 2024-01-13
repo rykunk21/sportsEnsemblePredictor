928c88b7b27eb61115c2394c0f93a9cc54865f1f
Ryan Kunkel <ryankunkel21@gmail.com>
2024-01-13 14:40:09 -0500

    added data handler skeleton

--- src/Lib/data.py
+++ src/Lib/data.py
@@ -4,14 +4,19 @@ GENERAL
 """
 
 class Preprocessor:
+    """
+    Class to provide general preprocessing tasks
+    """
     def __init__(self):
         pass
 
 
 class Handler:
+    """
+    Class to provide general data handling tasks
+    """
     def __init__(self):
         pass
-
 
 
 
@@ -21,14 +26,37 @@ SPECIAL HANDLERS
 """
 
 class languageHandler(Handler):
+    """
+    Data handler for language modeling
+    """
+
+    class languagePreprocessor(Preprocessor):
+        pass
+    
     def __init__(self):
         super().__init__()
 
+
 class simHandler(Handler):
+    """
+    Data handler for simulation
+    """
+    class simPreprocessor(Preprocessor):
+        pass
+
     def __init__(self):
         super().__init__()
 
 
 class powerHandler(Handler):
+    """
+    Data handler for power rankings
+    """
+    class powerPreprocessor(Preprocessor):
+        pass
+
     def __init__(self):
         super().__init__()
+
+
+
