0467f11612b3c2fa4705e20f7f56948c7a46c0b5
Ryan Kunkel <ryankunkel21@gmail.com>
2024-03-08 20:42:36 -0500

    doc

--- main.py
+++ main.py
@@ -22,10 +22,11 @@ def main():
     pull_parser = subparsers.add_parser('pull', help='Pull data with additional arguments')
     pull_parser.add_argument('teams', nargs='+', type=str, help='List of team numbers for pull command')
 
-
+    # Subpaser for 'update'
     update_parser = subparsers.add_parser('update', help='Update the existing list of teams or omit args to update all')
     update_parser.add_argument('teams', nargs='+', type=str, help='list of teams you wish ot update')
 
+    # Subparser for 'slate'
     update_parser = subparsers.add_parser('slate', help='Run a whole slate')
 
     # Parse the arguments
